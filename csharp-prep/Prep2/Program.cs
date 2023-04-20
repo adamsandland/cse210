1139511a4771f1fa35fae96380d8aa13153ee2c9
adamsandland <adam@sandlands.org>
2023-04-20 17:57:40 -0600

    Prep 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your current score? ");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+
+        string finalGrade = "0";
+        if(gradeInt>=90)
+        {
+            finalGrade = "A";
+        }
+        else if(gradeInt>=80)
+        {
+            finalGrade = "B";
+        }
+        else if(gradeInt >=70)
+        {
+            finalGrade = "C";
+        }
+        else if(gradeInt >=60)
+        {
+            finalGrade="D";
+        }
+        else{
+            finalGrade="F";
+        }
+        Console.WriteLine($"Your grade in the class is {finalGrade}");
     }
 }
