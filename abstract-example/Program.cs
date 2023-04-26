5ba5312e12569a9783ed567747996a7f61ebe15c
adamsandland <adam@sandlands.org>
2023-04-26 12:28:49 -0600

    made an example class

---
+++ abstract-example/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<book> book_list = new List<book>();
+
+
+book mistborn = new book("Mistborn", "Brandon Sanderson");
+book book_of_mormon = new book("Book of Mormon", "Moroni");
+
+mistborn.Display();
+book_of_mormon.Display();
+
+
+
+/*void make_booklist(string name, string author){
+    book new_book = new book(name, author);
+    book_list.Add(new_book);
+}*/
