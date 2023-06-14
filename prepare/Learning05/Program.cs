37714c1c92960f8caebe93339112a5493b913627
adamsandland <adam@sandlands.org>
2023-06-13 18:53:30 -0600

    Finished Learning Activity 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("red", 3.0));
+        shapes.Add(new Rectangle("yellow", 2.0, 4.0));
+        shapes.Add(new Circle("blue", 2.0));
+        foreach (var item in shapes)
+        {
+            Console.WriteLine($"{item.GetColor()}, {item.GetArea()}");
+        }
     }
 }
